cfca7103f8ab39d997716d4d299abddc11d6c4d2
Justin Kokosenski <Conchord.koko@gmail.com>
2015-10-31 15:52:44 -0700

    i love blimps on blimps

--- parsejson.py
+++ parsejson.py
@@ -4,24 +4,28 @@ import os
 
 jsonstream = urllib.urlopen("http://data1.jpl.nasa.gov/pds/imaging/msl/MSLHAZ_0XXX/DATA/?output=json.indented")
 JSONObject = json.load(jsonstream)
+#print(JSONObject['nodes'])
 
-sol_data = JSONObject['nodes'][2]
+sol_data = JSONObject['nodes']
 for x in range(0, len(sol_data)):
-    dir_name = os.mkdir(sol_data['name'])
+    solNameData = sol_data[x]
+    dir_name = solNameData['name']
+    os.mkdir(dir_name)
 
-print(sol_data['name'])
-solNameData = sol_data['name']
 
-result = urllib.urlopen("http://data1.jpl.nasa.gov/pds/imaging/msl/MSLHAZ_0XXX/DATA/" + solNameData + "?output=json.indented" )
+    print(dir_name)
+    result = urllib.urlopen("http://data1.jpl.nasa.gov/pds/imaging/msl/MSLHAZ_0XXX/DATA/" + dir_name + "?output=json.indented" )
 
-imageJsonObject = json.load(result)
 
-image_data = imageJsonObject['leaves']
+    imageJsonObject = json.load(result)
 
-for x in range(0, len(image_data)):
-    var = image_data[x]
-    image_string = var['name']
-    if image_string.endswith(".IMG") and "_F" in image_string:
-        url_result = urllib.urlretrieve("http://data1.jpl.nasa.gov/pds/imaging/msl/MSLHAZ_0XXX/DATA/" + solNameData + "/" + image_string + "/0/image[]?output=gif", dir_name/image_string[0:-4] + ".gif")
-        print(url_result)
-        print(image_string)
+    image_data = imageJsonObject['leaves']
+
+    for x in range(0, len(image_data)):
+        var = image_data[x]
+        image_string = var['name']
+
+        if image_string.endswith(".IMG") and "_F" in image_string:
+            url_result = urllib.urlretrieve("http://data1.jpl.nasa.gov/pds/imaging/msl/MSLHAZ_0XXX/DATA/" + dir_name + "/" + image_string + "/0/image[]?output=gif", dir_name + "/" + image_string[0:-4] + ".gif")
+            print(url_result)
+            print(image_string)
